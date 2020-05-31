1279e7f44332bb2f666c129d06cdb3bc743a1440
no1-bigcrab <nhiepphongts@gmail.com>
2020-05-31 23:23:17 +0700

    update image router

---
+++ Desktop/blog/app/Providers/layout/PostServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Providers\layout;
+
+use Illuminate\Support\ServiceProvider;
+
+class PostServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
